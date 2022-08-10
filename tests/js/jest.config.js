ce9438fb79f52c5ffe92986e4fd0922f04e90858
Ryan Kienstra <kienstraryan@gmail.com>
2022-08-10 17:36:23 -0500

    Delete needless values in jest.config.js

--- tests/js/jest.config.js
+++ tests/js/jest.config.js
@@ -1,23 +1,8 @@
 module.exports = {
 	rootDir: '../../',
 	...require( '@wordpress/scripts/config/jest-unit.config' ),
-	transform: {
-		'^.+\\.[jt]sx?$': '<rootDir>/node_modules/@wordpress/scripts/config/babel-transform',
-	},
 	"transformIgnorePatterns": [
 		"node_modules/(?!is-plain-obj)"
 	],
-	testPathIgnorePatterns: [
-		'<rootDir>/.git',
-		'<rootDir>/node_modules',
-	],
-	coveragePathIgnorePatterns: [
-		'<rootDir>/node_modules',
-	],
-	moduleNameMapper: {
-		'^react($|/.+)': '<rootDir>/node_modules/react$1'
-	},
-	coverageReporters: [ 'lcov' ],
-	coverageDirectory: '<rootDir>/coverage',
 	setupFilesAfterEnv: [ '<rootDir>/tests/js/jest.setup.js' ],
 };
